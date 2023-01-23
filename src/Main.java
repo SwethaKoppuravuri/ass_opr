be1513bd92ab07bf68583ee5aed85cb0cffb8690
SwethaKoppuravuri <119507872+SwethaKoppuravuri@users.noreply.github.com>
2023-01-23 07:23:34 +0530

    check assignment operators

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        int var;
+
+        // assign value using =
+        var = a;
+        System.out.println("var using =: " + var);
+
+        // assign value using =+
+        var += a;
+        System.out.println("var using +=: " + var);
+
+        // assign value using =*
+        var *= a;
+        System.out.println("var using *=: " + var);
+    }
+}
